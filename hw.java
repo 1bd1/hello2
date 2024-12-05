edc4ab49af98af79d93a080a0485728b0567a611
Dibran <dibran.ibisi@gmail.com>
2024-12-05 13:40:36 +0100

    [ticket no 2] druhy commit

--- hw.java
+++ hw.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World! omg");
+        System.out.println("Hello World! omg another chnage");
     }
 }
