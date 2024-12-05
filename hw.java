4a8d767b876d0a44176d9432d04351b8b7328973
Dibran <dibran.ibisi@gmail.com>
2024-12-05 13:47:09 +0100

    zmazanie slova World

--- hw.java
+++ hw.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World! omg another chnage");
+        System.out.println("Hello omg another chnage");
     }
 }
