18bb9d395a836e308dcfcf233031c4dde404c0df
Dibran <dibran.ibisi@gmail.com>
2024-12-05 15:15:48 +0100

    zduplikovanie kodu a oprava

--- hw.java
+++ hw.java
@@ -3,3 +3,10 @@ public class helloworld {
         System.out.println("Hello omg another chnage");
     }
 }
+
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
